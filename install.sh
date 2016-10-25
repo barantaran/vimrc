bfbfac1cf1d38580c48f88a89b97a25eb7770520
Dmitri Yourchev <d.yurchev@yandex.ru>
2016-10-25 12:33:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt-get install exuberant-ctags
-$ wget -O ~/.vimrc https://raw.githubusercontent.com/barantaran/vim-config/master/.vimrc
+wget -O ~/.vimrc https://raw.githubusercontent.com/barantaran/vim-config/master/.vimrc
 vim +PluginInstall +qa
